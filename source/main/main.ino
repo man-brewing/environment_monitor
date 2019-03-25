121417b09160596d82eb26a9fc2503349eb12c68
Nathan Dykstra <dykstra.nathan@gmail.com>
2019-03-25 12:24:54 -0500

    initial support for SD card reader and datalogging

--- source/main/main.ino
+++ source/main/main.ino
@@ -3,24 +3,30 @@
 */
 #include <Arduino.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
 #include "Adafruit_SHT31.h"
 
-Adafruit_SHT31 sht31 = Adafruit_SHT31();
-
-const int ledPin = 13;                  // the pin for the LED
+const int chipSelect = 10;               // SD card CS pin
 const int pirPin = 2;                   // the input pin for PIR sensor
 const int relayPin = 4;                 // connect the relay to this pin
 int motionSensorInput = 0;              // variable for reading the pin status
-long environmentInterval = 30 * 60000;  // minutes between environment readings
+long environmentInterval = 1 * 60000;  // minutes between environment readings
 long previousMillis = 0;
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 /*
  * Perform configuration and hardware setup when the board powers on.
  */
 void setup() {
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("Configuring board and sensors");
     
-  pinMode(ledPin, OUTPUT);      // declare LED as output
   pinMode(pirPin, INPUT);       // declare sensor as input
   pinMode(relayPin, OUTPUT);    // declare relay as output
 
@@ -30,6 +36,15 @@ void setup() {
     Serial.println("Couldn't find SHT31");
     while (1) delay(1);
   }
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+
+  Serial.println("Setup complete");
 }
 
 /*
@@ -39,20 +54,24 @@ void loop() {
   //read the input from the motion sensor
   motionSensorInput = digitalRead(pirPin);
 
-  //write the sensor output to the LED and relay
-  digitalWrite(ledPin, motionSensorInput);
+  //write the sensor output to the relay
   digitalWrite(relayPin, motionSensorInput);
 
-  //
+  //only read environment sensors at set intervals
   unsigned long currentMillis = millis();
   if(currentMillis - previousMillis > environmentInterval) 
   {
     previousMillis = currentMillis;
+
+    String dataString = "";
     
     float t = sht31.readTemperature();
     float h = sht31.readHumidity();
+
+    dataString = String(t) + "," + String(h);
+    Serial.println(dataString);    
   
-    if (!isnan(t)) 
+    if (!isnan(t))
     {
       Serial.print("Temp *C = "); 
       Serial.println(t);
@@ -69,6 +88,22 @@ void loop() {
     else
     { 
       Serial.println("Failed to read humidity");
+    }
+
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);// if the file is available, write to it:
+    if (dataFile)
+    {
+      dataFile.println(dataString);
+      dataFile.close();
+      // print to the serial port too:
+      Serial.println(dataString);
+    }
+    // if the file isn't open, pop up an error:
+    else
+    {
+      Serial.println("error opening datalog.txt");
     }
   }
 }
