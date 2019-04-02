8967f7a9fdcfbe5dfba9f222cbd49627cfb4195a
Nathan Dykstra <dykstra.nathan@gmail.com>
2019-04-01 22:54:22 -0500

    removed SD card support; enable Ethernet support and POSTing of environment data to home server

--- source/main/main.ino
+++ source/main/main.ino
@@ -2,64 +2,68 @@
  * @author: ndykstra
  * @date: 3/25/2019
  */
-#include <Arduino.h>
+//#include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <SD.h>
+// #include <SD.h>
 #include "Adafruit_SHT31.h"
+#include <Ethernet.h>
 
-const int chipSelect = 8;               // SD card CS pin
-const int pirPin = 9;                   // the input pin for PIR sensor
-const int relayPin = 7;                 // connect the relay to this pin
+const int pirPin = 8;                   // the input pin for PIR sensor
+const int relayPin = 9;                 // connect the relay to this pin
 int motionSensorInput = 0;              // variable for reading the pin status
-long environmentInterval = 1 * 60000;   // minutes between environment readings
+long environmentInterval = 1 * 5000;   // minutes between environment readings
 long previousMillis = 0;                // last time sensor was recorded
-bool serialDebuggingEnabled = false;    // enable/disable serial messaging
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+EthernetClient client;
+IPAddress server(192, 168, 1, 124);
+IPAddress ip(192, 168, 1, 130);
+IPAddress myDns(8, 8, 8, 8);
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 /*
  * Perform configuration and hardware setup when the board powers on.
  */
 void setup() {
-  if (serialDebuggingEnabled)
-  {
-    Serial.begin(9600);
-    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB port only
-    }
-
-    Serial.println("Configuring board and sensors");
+  Ethernet.init(10);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  Serial.println("Configuring board and sensors");
     
   pinMode(pirPin, INPUT);       // declare sensor as input
   pinMode(relayPin, OUTPUT);    // declare relay as output
 
   // make sure the temp/humid sensor is there
   if (!sht31.begin(0x44))
-  {
-    if (serialDebuggingEnabled)
-    {
-      Serial.println("Couldn't find SHT31");
-    }
-    
-    while (1) delay(1);
-  }
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    if (serialDebuggingEnabled)
-    {
-      Serial.println("Card failed, or not present");
-    }
-    
-    // don't do anything more:
+  {  
+    Serial.println("SHT31 not configured");
     while (1);
   }
-  if (serialDebuggingEnabled)
-  {
-    Serial.println("Setup complete");
+
+  Serial.println("SHT31 configured");
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip, myDns);
   }
+  
+  // give the Ethernet shield a second to initialize:
+  delay(3000);
+
+  // see if the card is present and can be initialized:
+//  if (!SD.begin(4)) {
+//    while (1);
+//  }
+
+  Serial.print("My IP is ");
+  Serial.println(Ethernet.localIP());
+  Serial.println("Setup complete");  
 }
 
 /*
@@ -76,52 +80,39 @@ void loop() {
   unsigned long currentMillis = millis();
   if(currentMillis - previousMillis > environmentInterval) 
   {
-    previousMillis = currentMillis;
-
-    String dataString = "";
-    
+    previousMillis = currentMillis;    
     float t = sht31.readTemperature();
     float h = sht31.readHumidity();
 
-    dataString = String(t) + "," + String(h);
-      
-    if (serialDebuggingEnabled)
-    {
-      if (!isnan(t))
-      {
-        Serial.print("Temp *C = "); 
-        Serial.println(t);
-      } 
-      else 
-      { 
-        Serial.println("Failed to read temperature");
-      }
-      
-      if (! isnan(h))
-      {  // check if 'is not a number'
-        Serial.print("Hum. % = "); Serial.println(h);
-      }
-      else
-      { 
-        Serial.println("Failed to read humidity");
-      }
-    }
-
     // open the file. note that only one file can be open at a time,
     // so you have to close this one before opening another.
-    File dataFile = SD.open("datalog.txt", FILE_WRITE);// if the file is available, write to it:
-    if (dataFile)
+//    File dataFile = SD.open("datalog.txt", FILE_WRITE);// if the file is available, write to it:
+//    if (dataFile)
+//    {
+//      dataFile.println(dataString);
+//      dataFile.close();
+//    }
+
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 3000))
     {
-      dataFile.println(dataString);
-      dataFile.close();
+      String postData = "temp=" + String(t) + "&humidity=" + String(h);
+      Serial.println("post");
+      client.println("POST /beerroom/environment HTTP/1.1");
+      client.println("User-Agent: arduino-ethernet");
+      client.println("Host: 192.168.1.115:3000");
+      client.println("Content-Type: application/x-www-form-urlencoded");
+      client.println("Connection:close");
+      client.print("Content-Length:");
+      client.println(postData.length());
+      client.println();
+      client.print(postData);
+    } else {
+      Serial.println("connection failed");
     }
-    // if the file isn't open, pop up an error:
-    else
-    {
-      if (serialDebuggingEnabled)
-      {
-        Serial.println("error opening datalog.txt");
-      }
+    if (!client.connected()) {
+      Serial.println("disconnecting.");
+      client.stop();
     }
   }
 }
